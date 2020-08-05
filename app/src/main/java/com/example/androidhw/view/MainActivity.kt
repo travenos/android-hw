4f031dbb84b271f786890378978f93598f08c127
Alexey Barashkov <barasher@yandex.ru>
2020-08-05 16:22:09 +0300

    Initial commit

---
+++ app/src/main/java/com/example/androidhw/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.androidhw.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.androidhw.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
