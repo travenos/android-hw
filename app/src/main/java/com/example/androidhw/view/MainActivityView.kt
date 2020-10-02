8fd5c9f09a7aeed063fd6820824c8875fa56e4a5
Alexey Barashkov <barasher@yandex.ru>
2020-10-02 11:11:13 +0300

    Fix import order

--- app/src/main/java/com/example/androidhw/view/MainActivityView.kt
+++ app/src/main/java/com/example/androidhw/view/MainActivityView.kt
@@ -1,7 +1,7 @@
 package com.example.androidhw.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.androidhw.MainActivityViewModel
 import com.example.androidhw.R
